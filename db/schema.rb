4f6ef14bf6df95cffbb89c21920930806d0425e1
Tom Hayden <tom.hayden@m4u.com.br>
2018-09-13 22:01:24 -0300

    adding crud for area

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_14_005951) do
+ActiveRecord::Schema.define(version: 2018_09_14_010103) do
+
+  create_table "areas", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
