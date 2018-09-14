7b787bf3294c1dd9f0c82837dd841b428139b67b
Tom Hayden <tom.hayden@m4u.com.br>
2018-09-14 10:22:50 -0300

    fixing issues while import a csv file with blank info

--- app/jobs/import_data_job.rb
+++ app/jobs/import_data_job.rb
@@ -14,13 +14,43 @@ class ImportDataJob < ApplicationJob
   private
 
   def create_record(row)
-    city = City.where(name: row['city']).first_or_create
-    skill = Skill.where(name: row['skill']).first_or_create do |skill|
-      skill.area = Area.where(name: row['area']).first_or_create
+    Hiring.create(name: row['name'].strip,
+                  city: create_city(row),
+                  skill: create_skill(row),
+                  year: row['year'].strip)
+  end
+
+  def create_city(row)
+    city_name = row['city']
+    return default_city if city_name.blank?
+    City.where(name: city_name.strip).first_or_create
+  end
+
+  def default_city
+    City.where(name: 'Unspecified').first_or_create
+  end
+
+  def create_skill(row)
+    skill_name = row['skill']
+    return default_skill if skill_name.blank?
+    skill = Skill.where(name: skill_name.strip).first_or_create do |skill|
+      skill.area = create_area(row)
     end
-    Hiring.create(name: row['name'],
-                  city: city,
-                  skill: skill,
-                  year: row['year'])
+  end
+
+  def default_skill
+    Skill.where(name: 'Unspecified').first_or_create do |skill|
+      skill.area = default_area
+    end
+  end
+
+  def create_area(row)
+    area_name = row['area']
+    return default_area if area_name.blank?
+    Area.where(name: area_name.strip).first_or_create
+  end
+
+  def default_area
+    Area.where(name: 'Unspecified').first_or_create
   end
 end
