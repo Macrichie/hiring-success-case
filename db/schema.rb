e7d7c5a7bb250d4c20cdba35c66bf184c7815943
Tom Hayden <tom.hayden@m4u.com.br>
2018-09-13 22:18:02 -0300

    adding crud for skills

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_14_010103) do
+ActiveRecord::Schema.define(version: 2018_09_14_010320) do
 
   create_table "areas", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2018_09_14_010103) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.integer "area_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["area_id"], name: "index_skills_on_area_id"
   end
 
 end
